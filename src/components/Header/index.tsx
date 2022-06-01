091766ad5e30ccb383e0f005ad2779dfa73dd3f9
amanda-santos <amandasf0200@gmail.com>
2022-06-01 13:27:24 -0300

    ✨ feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import { useRouter } from "next/router";
+import { ReactElement } from "react";
+
+import styles from "./header.module.scss";
+
+export default function Header(): ReactElement {
+  const router = useRouter();
+
+  return (
+    <header className={styles["header-container"]}>
+      <button type="button" onClick={() => router.push("/", {}, {})}>
+        <img src="/images/logo.svg" alt="logo" />
+      </button>
+    </header>
+  );
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,11 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+// import { GetStaticPaths, GetStaticProps } from "next";
+import { ReactElement } from "react";
+import Header from "../../components/Header";
 
-import { getPrismicClient } from '../../services/prismic';
+// import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
-import styles from './post.module.scss';
+// import commonStyles from '../../styles/common.module.scss';
+// import styles from './post.module.scss';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,9 +28,13 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(): ReactElement {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient({});
